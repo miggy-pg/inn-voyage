2ccf0f2163a37d5f13670801002a37847335183c
miggy-pg <meelan1323@gmail.com>
2023-10-20 04:00:35 +0800

    [UPDATE] add files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Heading from "./components/Heading";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Heading>Hello, world!</Heading>
+    </>
+  );
+}
+
+export default App;
