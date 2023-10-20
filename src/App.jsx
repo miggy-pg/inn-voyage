1546e4dd963b25662f7d7e4f04fb4d9c69aded1e
miggy-pg <meelan1323@gmail.com>
2023-10-21 00:01:03 +0800

    [UPDATE] wip template

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Heading from "./components/Heading";
+import Row from "./components/Row";
+import Button from "./components/Button";
 import { GlobalStyles } from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Heading>Hello, world!</Heading>
+      <Row type="horizontal">
+        <Heading as="h1">Hello, world!</Heading>
+        <Heading as="h2">
+          We are currently testing how styled-components work.
+        </Heading>
+        <Heading as="h3">So lets explore the documentation</Heading>
+      </Row>
+      <Button variations="medium" size="medium">
+        Test
+      </Button>
     </>
   );
 }
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,8 +1,30 @@
 import styled from "styled-components";
 
+// const test = css`
+//   text-align: center;
+// `;
+
 const Heading = styled.h1`
+  ${(props) =>
+    props.as === "h1" &&
+    `
+  font-size: 3.5rem;
+  font-weight: 700;
+`}
+  ${(props) =>
+    props.as === "h2" &&
+    `
   font-size: 2.5rem;
   font-weight: 700;
+`}
+  ${(props) =>
+    props.as === "h3" &&
+    `
+  font-size: 1.5rem;
+  font-weight: 700;
+`}
+
+  line-height: 1.5;
 `;
 
 export default Heading;
