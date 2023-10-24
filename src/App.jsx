23dc37196243abf085d44e1a074bb3b743d4d379
miggy-pg <meelan1323@gmail.com>
2023-10-24 23:26:51 +0800

    [UPDATE] wip create cabin form

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import CabinsPage from "./pages/CabinsPage";
 import AppLayout from "./components/AppLayout";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,6 +29,26 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 5000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "600px",
+            padding: "16px 24px",
+            background: "#363636",
+            color: "#fff",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
--- src/pages/CabinsPage.jsx
+++ src/pages/CabinsPage.jsx
@@ -1,6 +1,63 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import "../styles/global.css";
-import getCabins from "../services/apiCabins";
+import { getCabins, deleteCabin } from "../services/apiCabins";
+import toast from "react-hot-toast";
+
+function Cabin({ id: cabinId, name, maxCapacity, regularPrice, discount }) {
+  const queryClient = useQueryClient();
+
+  // we can immediately pass the API function from apiCabins.js to 'mutationFn'
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      toast.success("Successfully deleted cabin");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+    },
+    onError: (err) => toast.error(err.message), // the error message here is accessing the error message which is customizable
+  }); // from the API deleteCabin function (line 17)
+
+  return (
+    <div className="flex flex-wrap gap-4 ">
+      <div className="flex sm:col-span-12 md:col-span-8 sm:w-full md:w-8/12 lg:w-full items-center">
+        <div className="text-start ps-4">
+          {/* <h5 className="mb-3">Software Engineer</h5> */}
+          <span className="truncate me-3">
+            <i className="fa fa-map-marker-alt text-primary me-2"></i>
+            {name}
+          </span>
+          <span className="truncate me-3">
+            <i className="far fa-clock text-primary me-2"></i>
+          </span>
+          <span className="truncate me-0">
+            <i className="far fa-money-bill-alt text-primary me-2"></i>
+            {regularPrice}
+          </span>
+        </div>
+        <div className="sm:col-span-12 md:col-span-8 lg:col-span-12 sm:w-full md:w-4/12 lg:w-full flex flex-col items-start md:items-end justify-center">
+          <div className="flex mb-3">
+            <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+              Delete
+            </button>
+            <button
+              type="button"
+              className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-3xl px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900"
+            >
+              Book Cabin
+            </button>
+          </div>
+          <small className="truncate">
+            <i className="far fa-calendar-alt text-primary me-2"></i>
+            Capacity: {maxCapacity}
+          </small>
+          <small className="truncate">
+            <i className="far fa-calendar-alt text-primary me-2"></i>
+            Discount: {discount}
+          </small>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function Cabins() {
   // queryFn should return a promise
@@ -12,81 +69,29 @@ export default function Cabins() {
     queryKey: ["cabins"],
     queryFn: getCabins,
   });
+  console.log("isLoading: ", isLoading);
+  console.log("cabins: ", cabins);
+
   if (isLoading) return <div>Loading...</div>;
   return (
     <div className="w-4/5 py-5 mx-auto">
       <div className="container">
         <h1 className="text-center mb-5 ">Cabins</h1>
         <div className="tab-className text-center wow fadeInUp">
-          {/* <ul className="flex flex-wrap pl-0 mb-0 list-none justify-center border-b-[1px]">
-            <li className="flex-auto text-center">
-              <a
-                className="d-flex align-items-center text-start mx-3 ms-0 pb-3 active"
-                data-bs-toggle="pill"
-                href="#tab-1"
-              >
-                <h6 className="mt-n1 mb-0">All</h6>
-              </a>
-            </li>
-            <li className="flex-auto text-center">
-              <a
-                className="d-flex align-items-center text-start mx-3 pb-3"
-                data-bs-toggle="pill"
-                href="#tab-2"
-              >
-                <h6 className="mt-n1 mb-0">Booked</h6>
-              </a>
-            </li>
-            <li className="flex-auto text-center">
-              <a
-                className="d-flex align-items-center text-start mx-3 me-0 pb-3"
-                data-bs-toggle="pill"
-                href="#tab-3"
-              >
-                <h6 className="mt-n1 mb-0">Discounts</h6>
-              </a>
-            </li>
-          </ul> */}
           <div className="">
             <div className="p-0">
               <div className="border border-transparent rounded-lg shadow-md hover:shadow-lg transition duration-500 p-4 mb-4">
-                <div className="flex flex-wrap gap-4 ">
-                  <div className="flex sm:col-span-12 md:col-span-8 sm:w-full md:w-8/12 lg:w-full items-center">
-                    <div className="text-start ps-4">
-                      <h5 className="mb-3">Software Engineer</h5>
-                      <span className="truncate me-3">
-                        <i className="fa fa-map-marker-alt text-primary me-2"></i>
-                        New York, USA
-                      </span>
-                      <span className="truncate me-3">
-                        <i className="far fa-clock text-primary me-2"></i>Full
-                        Time
-                      </span>
-                      <span className="truncate me-0">
-                        <i className="far fa-money-bill-alt text-primary me-2"></i>
-                        $123 - $456
-                      </span>
-                    </div>
-                    <div className="sm:col-span-12 md:col-span-8 lg:col-span-12 sm:w-full md:w-4/12 lg:w-full flex flex-col items-start md:items-end justify-center">
-                      <div className="flex mb-3">
-                        <button
-                          type="button"
-                          className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-3xl px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900"
-                        >
-                          Book Cabin
-                        </button>
-                      </div>
-                      <small className="truncate">
-                        <i className="far fa-calendar-alt text-primary me-2"></i>
-                        Capacity: 4
-                      </small>
-                      <small className="truncate">
-                        <i className="far fa-calendar-alt text-primary me-2"></i>
-                        Bed: Queen
-                      </small>
-                    </div>
-                  </div>
-                </div>
+                {!isLoading &&
+                  cabins.map((cabin) => (
+                    <Cabin
+                      key={cabin.id}
+                      id={cabin.id}
+                      name={cabin.name}
+                      maxCapacity={cabin.maxCapacity}
+                      regularPrice={cabin.regularPrice}
+                      discount={cabin.discount}
+                    />
+                  ))}
               </div>
             </div>
           </div>
